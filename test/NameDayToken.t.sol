cf796e2cd6189f1ae608b0643b4ebb0eee65d805
MartinGbz <martin.grabarz@outlook.fr>
2023-12-04 11:45:04 +0100

    test: now test need to be done on sepolia. Switched from aliceToken to martinToken because I control all the ens that are on the test to ensure they won't fail

--- test/NameDayToken.t.sol
+++ test/NameDayToken.t.sol
@@ -5,110 +5,120 @@ import "forge-std/Test.sol";
 import "../src/NameDayToken.sol";
 import "forge-std/console.sol";
 
-contract NameDayTokenTest is Test {
-    NameDayToken public aliceToken;
+/**
+    ALL TESTS NEED TO BE RUN WITH USING SEPOLIA TESTNET
+    forge test --fork-url https://ethereum-sepolia.publicnode.com
 
-    address alice = address(0xcd2E72aEBe2A203b84f46DEEC948E6465dB51c75);
-    address alice1 = address(0x3f125f040a5AA108C8E136c5d671895e533742E9);
-    address alicecooper = address(0xa54fb799525Ac436F8bf3d88b3FA241A4e9e2599);
-    address martingbz = address(0x4801eB5a2A6E2D04F019098364878c70a05158F1);
+    I control all the ENS names used in the tests on the sepolia testnet
+    This ensures that the tests will always pass
+    Otherwise, the tests would fail if someone changed the resolver of the ENS names used in the tests
+ */
+
+contract NameDayTokenTest is Test {
+    NameDayToken public martinToken;
+
+    address martin = address(0x4801eB5a2A6E2D04F019098364878c70a05158F1);
+    address martin1 = address(0x4801eB5a2A6E2D04F019098364878c70a05158F1);
+    address martin2 = address(0xFa3ED20a82df27DF4b1a01dfb7EFC9b1b0848241);
+    address alice2 = address(0xFa3ED20a82df27DF4b1a01dfb7EFC9b1b0848241);
 
     uint256 DAY_IN_SECONDS = 24 * 60 * 60;
     uint256 MAX_INT_TYPE = type(uint256).max;
 
     function setUp() public {
-        // 16/12/2023 : 0am
-        vm.warp(1702684800);
-        aliceToken = new NameDayToken("AliceToken", "ALICE", "alice", 1702684800, 100, 1e24);
+        // 11/11/2023 : 0am
+        vm.warp(1699660800);
+        martinToken = new NameDayToken("MartinToken", "MARTIN", "martin", 1699660800, 100, 1e24);
     }
 
     function testConstructor() public {
-        console.log("name : %s", aliceToken.name());
+        console.log("name : %s", martinToken.name());
         console.log(block.timestamp);
-        assertEq(aliceToken.name(), "AliceToken");
-        assertEq(aliceToken.totalSupply(), 0);
+        assertEq(martinToken.name(), "MartinToken");
+        assertEq(martinToken.totalSupply(), 0);
     }
-
-    // /!\ Tests can fail because:
-    // - if owners of alice*.eth remove resolving address, the test will fail
-    // - if the alice*.eth owner changes, the test will fail too
 
     /*---------- MINT TESTS ----------*/
 
     function testMintSuccess() public {
-        // vm.warp(1702684800);
-        vm.startPrank(alice);
-        aliceToken.mint("alice");
+        vm.startPrank(martin);
+        martinToken.mint("martin");
     }
 
     function testMintSuccessOtherTime() public {
-        // 16/12/2023 : 13am
-        vm.warp(1702731600);
-        vm.startPrank(alice);
-        aliceToken.mint("alice");
+        // 11/11/2023 : 13am
+        vm.warp(1699707600);
+        vm.startPrank(martin);
+        martinToken.mint("martin");
     }
 
     function testMintSuccessOtherName() public {
-        vm.startPrank(alicecooper);
-        aliceToken.mint("alicecooper");
+        vm.startPrank(martin2);
+        martinToken.mint("martin2");
     }
 
-    function testMintFailNoResolvingAddress() public {
-        // at the time of the test (17/11/2023) alice1.eth has no resolving address
-        vm.startPrank(alice1);
-        vm.expectRevert(bytes("Resolver has not been found"));
-        aliceToken.mint("alice1");
+    // // at the time of the test (17/11/2023) alice1.eth has no resolving address on ethereum mainnet
+    // function testMintFailNoResolvingAddress() public {
+    //     vm.startPrank(alice1);
+    //     vm.expectRevert(bytes("Resolver has not been found"));
+    //     aliceToken.mint("alice1");
+    // }
+    
+    function testMintFailNoETHRecordAddress() public {
+        vm.startPrank(martin1);
+        vm.expectRevert(bytes("Only the owner of the ENS name can mint tokens"));
+        martinToken.mint("martin1");
     }
 
     function testMintFailSupply() public {
-        NameDayToken aliceToken2 = new NameDayToken("AliceToken", "ALICE", "alice", 1702684800, 100, 150);
-        vm.startPrank(alice);
-        aliceToken2.mint("alice");
-        vm.startPrank(alicecooper);
+        NameDayToken aliceToken2 = new NameDayToken("MartinToken", "MARTIN", "martin", 1699660800, 100, 150);
+        vm.startPrank(martin);
+        aliceToken2.mint("martin");
+        vm.startPrank(martin2);
         vm.expectRevert(bytes("Max supply reached"));
-        aliceToken2.mint("alicecooper");
+        aliceToken2.mint("martin2");
     }
 
     function testMintFailDateBefore() public {
-        // 1 day before : 15/12/2023
-        vm.warp(1702636498);
-        vm.startPrank(alice);
-        vm.expectRevert(bytes("Transfers are only allowed on alice's day"));
-        aliceToken.mint("alice");
+        // 1 day before : 10/11/2023
+        vm.warp(1699574400);
+        vm.startPrank(martin);
+        vm.expectRevert(bytes("Transfers are only allowed on martin's day"));
+        martinToken.mint("martin");
     }
 
     function testMintFailDateAfter() public {
-        // 1 day later : 17/12/2023
-        vm.warp(1702771200);
-        vm.startPrank(alice);
-        vm.expectRevert(bytes("Transfers are only allowed on alice's day"));
-        aliceToken.mint("alice");
+        // 1 day later : 12/11/2023
+        vm.warp(1699747200);
+        vm.startPrank(martin);
+        vm.expectRevert(bytes("Transfers are only allowed on martin's day"));
+        martinToken.mint("martin");
     }
 
     function testMintFailUserNotAllowed() public {
-        vm.startPrank(martingbz);
-        vm.expectRevert(bytes("Only an owner of an ENS name that contains alice can mint tokens"));
-        aliceToken.mint("martingbz");
+        vm.startPrank(alice2);
+        vm.expectRevert(bytes("Only an owner of an ENS name that contains martin can mint tokens"));
+        martinToken.mint("alice2");
     }
 
     function testMintFailImpersonate() public {
-        vm.startPrank(martingbz);
+        vm.startPrank(alice2);
         vm.expectRevert(bytes("Only the owner of the ENS name can mint tokens"));
-        aliceToken.mint("alice");
+        martinToken.mint("martin");
     }
 
     function testDoubleMintInADay() public {
-        vm.startPrank(alice);
-        aliceToken.mint("alice");
+        vm.startPrank(martin);
+        martinToken.mint("martin");
         vm.expectRevert(bytes("You already minted tokens this year"));
-        aliceToken.mint("alice");
+        martinToken.mint("martin");
     }
 
     function testMintSuccessAfterLeapYear() public {
-        // 16/12/25 : 5am
-        vm.warp(1765857600);
-        vm.startPrank(alice);
-        aliceToken.mint("alice");
+        // 11/11/25 : 5am
+        vm.warp(1762837200);
+        vm.startPrank(martin);
+        martinToken.mint("martin");
     }
 
     function testFuzz_Mint(uint256 currentTimeStamp, uint256 nameDayTimeStamp) public {
@@ -116,108 +126,107 @@ contract NameDayTokenTest is Test {
         vm.assume(currentTimeStamp <= MAX_INT_TYPE - DAY_IN_SECONDS);
 
         vm.warp(currentTimeStamp);
-        NameDayToken aliceToken2 = new NameDayToken("AliceToken2", "ALICE2", "alice", nameDayTimeStamp, 100, 1e24);
-        vm.startPrank(alice);
+        NameDayToken martinToken2 = new NameDayToken("MartinToken2", "MARTIN2", "martin", nameDayTimeStamp, 100, 1e24);
+        vm.startPrank(martin);
         if(currentTimeStamp >= nameDayTimeStamp && currentTimeStamp < nameDayTimeStamp+DAY_IN_SECONDS) {
-            aliceToken2.mint("alice");
+            martinToken2.mint("martin");
         } else {
-            vm.expectRevert(bytes("Transfers are only allowed on alice's day"));
-            aliceToken2.mint("alice");
+            vm.expectRevert(bytes("Transfers are only allowed on martin's day"));
+            martinToken2.mint("martin");
         }
     }
 
     /*---------- TRANSFERT TESTS ----------*/
 
     function testMintAndTransferSuccess() public {
-        vm.startPrank(alice);
-        aliceToken.mint("alice");
-        aliceToken.transfer(martingbz, 50);
-        assertEq(aliceToken.balanceOf(alice), 50);
-        assertEq(aliceToken.balanceOf(martingbz), 50);
-        assertEq(aliceToken.totalSupply(), 100);
+        vm.startPrank(martin);
+        martinToken.mint("martin");
+        martinToken.transfer(alice2, 50);
+        assertEq(martinToken.balanceOf(martin), 50);
+        assertEq(martinToken.balanceOf(alice2), 50);
+        assertEq(martinToken.totalSupply(), 100);
     }
 
     function testMintAndApproveAndTransferFromSuccess() public {
-        vm.startPrank(alice);
-        aliceToken.mint("alice");
-        aliceToken.approve(martingbz, 50);
+        vm.startPrank(martin);
+        martinToken.mint("martin");
+        martinToken.approve(alice2, 50);
 
-        vm.startPrank(martingbz);
-        aliceToken.transferFrom(alice, martingbz, 50);
+        vm.startPrank(alice2);
+        martinToken.transferFrom(martin, alice2, 50);
 
-        assertEq(aliceToken.balanceOf(alice), 50);
-        assertEq(aliceToken.balanceOf(martingbz), 50);
-        assertEq(aliceToken.totalSupply(), 100);
+        assertEq(martinToken.balanceOf(martin), 50);
+        assertEq(martinToken.balanceOf(alice2), 50);
+        assertEq(martinToken.totalSupply(), 100);
     }
 
     function testMintAndTransferFailDate() public {
-        vm.startPrank(alice);
-        aliceToken.mint("alice");
+        vm.startPrank(martin);
+        martinToken.mint("martin");
 
-        // 17/12/2023 : 10am
-        vm.warp(1702807200);
-        vm.expectRevert(bytes("Transfers are only allowed on alice's day"));
-        aliceToken.transfer(martingbz, 50);
+        // 1 day later: 12/11/2023 : 10am
+        vm.warp(1699783200);
+        vm.expectRevert(bytes("Transfers are only allowed on martin's day"));
+        martinToken.transfer(alice2, 50);
     }
 
      function testMintAndApproveAndTransferFromFailDate() public {
-        vm.startPrank(alice);
-        aliceToken.mint("alice");
+        vm.startPrank(martin);
+        martinToken.mint("martin");
 
         // 10/04/2024 : 15am
         vm.warp(1712761200);
-        aliceToken.approve(martingbz, 50); // should not revert
+        martinToken.approve(alice2, 50); // should not revert
 
-        vm.startPrank(martingbz);
-        vm.expectRevert(bytes("Transfers are only allowed on alice's day"));
-        aliceToken.transferFrom(alice, martingbz, 50);
+        vm.startPrank(alice2);
+        vm.expectRevert(bytes("Transfers are only allowed on martin's day"));
+        martinToken.transferFrom(martin, alice2, 50);
     }
 
     /*---------- GET FUNCTION TESTS ----------*/
 
     function testGetBaseYear() public {
-        uint256 baseTimestamp = aliceToken.getBaseTimestamp();
-        assertEq(baseTimestamp, 1702684800);
+        uint256 baseTimestamp = martinToken.getBaseTimestamp();
+        assertEq(baseTimestamp, 1699660800);
     }
 
     function testGetCurrentYearNameDayTimestamp() public {
         // 01/02/2024 : 0am
         vm.warp(1706745600);
-        uint256 timestamp = aliceToken.getCurrentYearNameDayTimestamp();
+        uint256 timestamp = martinToken.getCurrentYearNameDayTimestamp();
 
         // timestamp should be 16/12/2024 : 0am
-        assertEq(timestamp, 1734307200);
+        assertEq(timestamp, 1731283200);
     }
 
     function testHasMinted() public {
-        vm.startPrank(alice);
-        aliceToken.mint("alice");
-        assertEq(aliceToken.hasMinted(2023, "alice"), true);
-        assertEq(aliceToken.hasMinted(2023, "alice1"), false);
-        assertEq(aliceToken.hasMinted(2024, "alice"), false);
+        vm.startPrank(martin);
+        martinToken.mint("martin");
+        assertEq(martinToken.hasMinted(2023, "martin"), true);
+        assertEq(martinToken.hasMinted(2023, "martin2"), false);
+        assertEq(martinToken.hasMinted(2024, "martin"), false);
 
-        // 16/12/2025 : 0am
-        vm.warp(1765843200);
-        aliceToken.mint("alice");
-        assertEq(aliceToken.hasMinted(2025, "alice"), true);
-        assertEq(aliceToken.hasMinted(2024, "alice"), false);
+        // 11/11/2025 : 0am
+        vm.warp(1762819200);
+        martinToken.mint("martin");
+        assertEq(martinToken.hasMinted(2025, "martin"), true);
+        assertEq(martinToken.hasMinted(2024, "martin"), false);
     }
 
-
     function testGetUserMints() public {
-        vm.startPrank(alice);
+        vm.startPrank(martin);
         
-        aliceToken.mint("alice");
+        martinToken.mint("martin");
         
-        // 16/12/2025 : 0am
-        vm.warp(1765843200);
-        aliceToken.mint("alice");
+        // 11/11/2025 : 0am
+        vm.warp(1762819200);
+        martinToken.mint("martin");
         
-        // 16/12/2026 : 0am
-        vm.warp(1797379200);
-        aliceToken.mint("alice");
+        // 11/11/2026 : 0am
+        vm.warp(1794355200);
+        martinToken.mint("martin");
 
-        bool[] memory mints = aliceToken.getUserMints("alice");
+        bool[] memory mints = martinToken.getUserMints("martin");
         assertTrue(
             mints[0] == true &&
             mints[1] == false &&
@@ -232,11 +241,11 @@ contract NameDayTokenTest is Test {
     event Mint(uint256 indexed year, string indexed ensName);
 
     function testMintEvent() public {
-        vm.startPrank(alice);
+        vm.startPrank(martin);
 
         vm.expectEmit(true, true, false, false);
-        emit Mint(2023, "alice");
+        emit Mint(2023, "martin");
 
-        aliceToken.mint("alice");
+        martinToken.mint("martin");
     }
 }
